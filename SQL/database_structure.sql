55773bb18dce47e5daed35e1f8dddc01817477c3
Moustafa Ismail <moustafaism013@hotmail.com>
2022-09-16 22:06:47 +0300

    Create database_structure.sql

--- SQL/database_structure.sql
+++ SQL/database_structure.sql
@@ -10,7 +10,7 @@ USE `twitterdb` ;
 CREATE TABLE IF NOT EXISTS `twitterdb`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NO NULL,
+  `email` VARCHAR(255) NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `date_of_birth` DATE NOT NULL,
