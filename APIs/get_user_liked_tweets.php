9662e267601b481436c74b8f951f9092ddc35a25
Moustafa <moustafaism013@hotmail.com>
2022-09-17 09:00:55 +0300

    Finished the get_user_like_tweets api

--- APIs/get_user_liked_tweets.php
+++ APIs/get_user_liked_tweets.php
@@ -7,7 +7,16 @@
 
     $query = $conn->prepare("SELECT `u`.`username`, `u`.`name`, `t`.`tweet_text`, `t`.`tweet_image_link`, `t`.`date_time_of_creation`
     FROM `likes` AS l, `tweets` AS t, `users` AS u
-    WHERE `l`.`tweet_id`=`t`.`id` AND `u`.`id`=`t`.`user_id` AND `l`.`user_id`=2;");
-    
+    WHERE `l`.`tweet_id`=`t`.`id` AND `u`.`id`=`t`.`user_id` AND `l`.`user_id`=?");
+    $query->bind_param("i", $userId);
+    $query->execute();
+    $result = $query->get_result();
 
+    $response = [];
+
+    while($row = $result->fetch_assoc()) {
+        $response["liked"][] = $row;
+    }
+
+    echo json_encode($response);
 ?>
