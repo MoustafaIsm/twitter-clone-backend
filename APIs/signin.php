edc1a8f9f3c9a8b312a8581ba4819d94795f80df
JaafarHawli <jaafarhawle@gmail.com>
2022-09-18 01:41:06 +0300

    updated signin api to return data only if email and password are valid

--- APIs/signin.php
+++ APIs/signin.php
@@ -7,16 +7,35 @@ include("connection.php");
 
 $email = $_POST["email"];
 $password = hash("sha256",$_POST["password"]);
-
-$query = $mysqli->prepare("SELECT * FROM users WHERE email=? AND password=?");
-$query-> bind_param("ss", $email, $password);
-$query->execute();
-$result = $query->get_result();
-
 $response = [];
 
-while($a = $result->fetch_assoc()){
-    $response[] = $a;
+$query = $mysqli->prepare("SELECT email FROM users WHERE email=?");
+$query-> bind_param("s", $email);
+$query->execute();
+$result = $query->get_result();
+if(mysqli_num_rows($result)==0) {
+    $response["email"] = false;  
+}
+else {
+    
+    $query = $mysqli->prepare("SELECT email, password FROM users WHERE email=? AND password=?");
+    $query-> bind_param("ss", $email, $password);
+    $query->execute();
+    $result = $query->get_result();
+    if(mysqli_num_rows($result)>0) {
+        $query = $mysqli->prepare("SELECT * FROM users WHERE email=? AND password=?");
+        $query-> bind_param("ss", $email, $password);
+        $query->execute();
+        $result = $query->get_result(); 
+        
+        while($a = $result->fetch_assoc()){
+            $response[] = $a;
+        }
+    }
+    else {
+        $response["email"] = true;
+        $response["password"] = false;
+    }
 }
 
 $json = json_encode($response);
