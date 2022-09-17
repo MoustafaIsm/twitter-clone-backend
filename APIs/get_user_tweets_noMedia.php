5b7ecf5173ea20864c5dcf83ee873baff1af836f
Moustafa <moustafaism013@hotmail.com>
2022-09-17 08:12:06 +0300

    Finished get_user_tweet_noMedia api

--- APIs/get_user_tweets_noMedia.php
+++ APIs/get_user_tweets_noMedia.php
@@ -6,7 +6,16 @@
     $userId = $_GET["userId"];
 
     $query = $conn->prepare("SELECT `tweet_text`, `date_time_of_creation` FROM `tweets` WHERE `user_id`=? AND `tweet_image_link`= 'NA'");
-    $query>bind_param("i", $userId);
-    $result = $query->execute();
+    $query->bind_param("i", $userId);
+    $query->execute();
+    $result = $query->get_result();
+
+    $response = [];
+
+    while($row = $result->fetch_assoc()) {
+        $response["tweets"][] = $row;
+    }
+
+    echo json_encode($response);
 
 ?>
