64ee06dede6c97a6f2b6c2ee1908aac7f09a62af
Moustafa <moustafaism013@hotmail.com>
2022-09-17 08:31:59 +0300

    Finished get_following_tweets api

--- APIs/get_following_tweets.php
+++ APIs/get_following_tweets.php
@@ -3,8 +3,22 @@
 
     include("./connection.php");
 
+    $userId = $_GET["userId"];
 
-    
+    $query = $conn->prepare("SELECT `u`.`username`, `u`.`name`, `t`.`tweet_text`, `t`.`tweet_image_link`, `t`.`date_time_of_creation`  
+    FROM `following` AS f, `tweets` AS t, `users` AS u
+    WHERE `f`.`following_user_id` = `t`.`user_id` AND `f`.`following_user_id`= `u`.`id`  AND `f`.`users_id`=?");
+    $query->bind_param("i", $userId);
+    $query->execute();
+    $result = $query->get_result();
+
+    $response = [];
+
+    while($row = $result->fetch_assoc()) {
+        $response["feeds"][] = $row;
+    }
+
+    echo json_encode($response);
 
 
 ?>
