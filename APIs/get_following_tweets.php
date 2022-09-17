994d79ba8cda8130dff9e757084fb07530e28fbf
Moustafa <moustafaism013@hotmail.com>
2022-09-17 08:43:31 +0300

    Fixed the query for get_user_following api

--- APIs/get_following_tweets.php
+++ APIs/get_following_tweets.php
@@ -7,7 +7,7 @@
 
     $query = $conn->prepare("SELECT `u`.`username`, `u`.`name`, `t`.`tweet_text`, `t`.`tweet_image_link`, `t`.`date_time_of_creation`  
     FROM `following` AS f, `tweets` AS t, `users` AS u
-    WHERE `f`.`following_user_id` = `t`.`user_id` AND `f`.`following_user_id`= `u`.`id`  AND `f`.`users_id`=?");
+    WHERE `f`.`following_user_id` = `t`.`user_id` AND `f`.`following_user_id`= `u`.`id`  AND `f`.`user_id`=?");
     $query->bind_param("i", $userId);
     $query->execute();
     $result = $query->get_result();
@@ -19,6 +19,4 @@
     }
 
     echo json_encode($response);
-
-
 ?>
--- APIs/get_user_following.php
+++ APIs/get_user_following.php
@@ -7,7 +7,16 @@
 
     $query = $conn->prepare("SELECT `u`.`id`, `u`.`username`, `u`.`name`
     FROM `following` AS f, `users` AS u
-    WHERE `f`.`following_user_id`=`u`.`id`AND `f`.`users_id`=?");
-    
+    WHERE `f`.`following_user_id`=`u`.`id`AND `f`.`user_id`=?");
+    $query->bind_param("i", $userId);
+    $query->execute();
+    $result = $query->get_result();
 
+    $response = [];
+
+    while($row = $result->fetch_assoc()) {
+        $response["following"][] = $row;
+    }
+
+    echo json_encode($response);
 ?>
