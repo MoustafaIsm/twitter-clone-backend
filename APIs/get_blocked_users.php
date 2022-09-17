7d6f9be42013be7674b23e7e70fdd822857a14b0
Moustafa <moustafaism013@hotmail.com>
2022-09-17 13:11:59 +0300

    Finished get blocked users api and tested it

--- APIs/get_blocked_users.php
+++ APIs/get_blocked_users.php
@@ -6,5 +6,15 @@
     $userId = $_GET["userId"];
 
     $query = $conn->prepare("SELECT `u`.`username`, `u`.`name` FROM `users` AS u, `blocked` AS b WHERE `u`.`id`=`b`.`blocked_user_id` AND `b`.`user_id`=?");
-    
->?
+    $query->bind_param("i", $userId);
+    $query->execute();
+    $result = $query->get_result();
+
+    $response = [];
+
+    while($row = $result->fetch_assoc()) {
+        $response["blocked-users"][] = $row;
+    }
+
+    echo json_encode($response);
+?>
